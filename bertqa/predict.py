5c2868693675ef1b4b2cfb29d97fbed749222eb5
hr9397 <hr9397@rit.edu>
2022-10-11 20:36:44 -0400

    adding code for getting answers for all news channels

--- bertqa/predict.py
+++ bertqa/predict.py
@@ -4,23 +4,22 @@ import pandas as pd
 from ast import literal_eval
 cur_wd = os.getcwd()
 sys.path.insert(0,'bertqa')
-
-
+from tqdm import tqdm, tqdm_notebook
+tqdm.pandas()
 from cdqa.pipeline.cdqa_sklearn import QAPipeline
 from cdqa.utils.filters import filter_paragraphs
 
-CHANNEL = "fox"
-DATA_DIR = "/Users/harshitrajgarhia/PycharmProjects/DSCI789/data/"
-FILTERED_DATA_DIR = DATA_DIR+ os.sep + "filtered_data"
 reader_path = os.path.join(cur_wd,'bertqa/models/bert_qa.joblib')
+DATA_DIR = "/Users/harshitrajgarhia/PycharmProjects/DSCI789/data"
+FILTERED_DATA_DIR = DATA_DIR + os.sep + "filtered_data"
 
 
 def fetch_cdqa_pipeline(reader_path):
     cdqa_pipeline = QAPipeline(reader=reader_path)
     return cdqa_pipeline
 
-def build_knowledge_base(ip_path):
-    filename = ip_path+os.sep+"qna_data"+os.sep+CHANNEL+"_video_qna_data.csv"
+def build_knowledge_base(channel,ip_path):
+    filename = ip_path+os.sep+"qna_data"+os.sep+channel+"_video_qna_data.csv"
     df = pd.read_csv(filename, converters={'paragraphs': literal_eval})
     #df = filter_paragraphs(df)
     return df
@@ -34,9 +33,9 @@ def predict_answer(query,cdqa_pipeline,num_answers):
     predictions = cdqa_pipeline.predict(query=query,n_predictions=num_answers)
     for i,prediction in enumerate(predictions):
         prediction = list(prediction)
-        result = {'Rank':(i+1),'answer': prediction[0],'title': prediction[1],'paragraph':prediction[2]}
+        result = {'Rank':(i+1),'answer': prediction[0],'title': prediction[1],'paragraph':prediction[2],'score':prediction[3]}
         results.append(result)
-    print(results)
+    #print(results)
     return results
 
 def get_answer(df,cdqa_pipeline, query,num_answers):
@@ -45,43 +44,45 @@ def get_answer(df,cdqa_pipeline, query,num_answers):
     results = predict_answer(query,cdqa_pipeline,num_answers)
     return results
 
+def get_top_n_answers(row,number,cdqa_pipeline):
+    query = row['query']
+    channel_list = ["fox","cnn","msnbc"]
+    #channel_list = ["fox"]
+    for channel in channel_list:
+        filtered_video_df = pd.read_hdf(FILTERED_DATA_DIR + os.sep + "csv_files" + os.sep + channel + '_video.hdf', channel + '_video_df')
+        title_id_dict = dict(filtered_video_df[['title', 'id']].values)
+        channel_corpus_df = build_knowledge_base(channel,FILTERED_DATA_DIR)
+        cdqa_pipeline.fit_retriever(df=channel_corpus_df)
+        answerlist = get_answer(channel_corpus_df, cdqa_pipeline, query, number)
+        for i, answer in enumerate(answerlist):
+            row[channel+"_answer_" + str(i + 1)] = answer['answer']
+            row[channel+"_title_" + str(i + 1)] = answer['title']
+            row[channel+"_paragraph_" + str(i + 1)] = answer['paragraph']
+            row[channel+"_score_" + str(i + 1)] = answer['score']
+            row[channel + "_id_" + str(i + 1)] = title_id_dict[answer['title']]
+    return row
+
+
+
+def get_answers_across_channels(questions_list,number):
+    cdqa_pipeline = fetch_cdqa_pipeline(reader_path)
+    result_df = pd.DataFrame()
+    result_df['query'] = questions_list
+    result_df_across_channels = result_df.progress_apply(lambda x:get_top_n_answers(x,number,cdqa_pipeline),axis=1)
+    return result_df_across_channels
+
 if __name__=='__main__':
+    '''
     cdqa_pipeline = fetch_cdqa_pipeline(reader_path)
     df = build_knowledge_base(FILTERED_DATA_DIR)
     #get_answer(df,cdqa_pipeline,'who has been affected by the coronovirus in indian army',3)
     a = get_answer(df,cdqa_pipeline,'When does life starts?',10)
+    '''
 
+    questions_list = ["When does life starts?", "Is Abortion a murder?"]
+    result_df = get_answers_across_channels(questions_list, 5)
+    answers_columns = [column for column in result_df.columns if 'answer' in column or 'query' in column]
+    result_df_answers = result_df[answers_columns]
+    result_df_answers.to_csv(FILTERED_DATA_DIR+os.sep+"qna_results"+os.sep+"qna_results_across_channels.csv",index=False)
+    result_df.to_csv(FILTERED_DATA_DIR+os.sep+"qna_results"+os.sep+"qna_results_across_channels_metadata.csv",index=False)
 
-
-
-'''
-import os
-import pandas as pd
-
-from cdqa.utils.filters import filter_paragraphs
-from cdqa.pipeline import QAPipeline
-
-
-from cdqa.utils.download import download_model, download_bnpp_data, download_squad
-
-download_bnpp_data(dir='./data/bnpp_newsroom_v1.1/')
-download_model(model='bert-squad_1.1', dir='./models')
-download_squad(dir='./data/squad_v1.1/')
-
-
-df = pd.read_csv('./data/bnpp_newsroom_v1.1/bnpp_newsroom-v1.1.csv', converters={'paragraphs': literal_eval})
-df = filter_paragraphs(df)
-cdqa_pipeline = QAPipeline(reader='models/bert_qa.joblib') # use 'distilbert_qa.joblib' for DistilBERT instead of BERT
-cdqa_pipeline.fit_retriever(df=df)
-
-query = 'Since when does the Excellence Program of BNP Paribas exist?'
-prediction = cdqa_pipeline.predict(query)
-'''
-
-from cdqa.utils.converters import pdf_converter
-import pandas as pd
-from ast import literal_eval
-
-df = pd.read_csv('./data/bnpp_newsroom_v1.1/bnpp_newsroom-v1.1.csv', converters={'paragraphs': literal_eval})
-df = df[['title','paragraphs']]
-df = filter_paragraphs(df)
--- data_staging/data_extraction.py
+++ data_staging/data_extraction.py
@@ -6,7 +6,7 @@ from collections import ChainMap
 import numpy as np
 import string
 
-CHANNEL = "fox"
+CHANNEL = "msnbc"
 DATA_DIR = "/Users/harshitrajgarhia/PycharmProjects/DSCI789/data/"
 CHANNEL_DIR = DATA_DIR + os.sep + CHANNEL + "_parsed"
 METADATA_DIR = CHANNEL_DIR + os.sep + "metadata"
@@ -14,22 +14,30 @@ JSON_BY_LINE_DIR = CHANNEL_DIR + os.sep + "jsonbyline"
 VIDEO_DATA_DIR = DATA_DIR + os.sep + "DSCI-789-84740"
 FILTERED_DATA_DIR = DATA_DIR+ os.sep + "filtered_data"
 
-filtered_comments_df = pd.read_hdf(FILTERED_DATA_DIR+os.sep+"csv_files"+os.sep+CHANNEL+'_comments.hdf',CHANNEL+'_df')
+#filtered_comments_df = pd.read_hdf(FILTERED_DATA_DIR+os.sep+"csv_files"+os.sep+CHANNEL+'_comments.hdf',CHANNEL+'_df')
 filtered_video_df = pd.read_hdf(FILTERED_DATA_DIR+os.sep+"csv_files"+os.sep+CHANNEL+'_video.hdf',CHANNEL+'_video_df')
 
 def get_paragraphs(row):
     video_id = row['id']
+    #video_id = 'CuhMGxOPxR4'
+    print("******************* "+video_id)
     video_comments_list = []
     video_id_list_with_no_comments = []
     video_comments_list_text = []
     try:
-        with open(JSON_BY_LINE_DIR + os.sep + video_id + "_jsonbyline.txt") as f:
+        with open(JSON_BY_LINE_DIR + os.sep + video_id + "_jsonbyline.txt",'rb') as f:
             for line in f:
-                video_comments_list.append(ast.literal_eval(line))
+                print(line)
+                dict_str = line.decode('utf-8')  # converting from byte class to utf-8
+                dict_str = ast.literal_eval(dict_str)  # Evaluates the string can be parsed or not
+                dict_str['text'] = dict_str['text'].lower()  # converts the text to lowercase
+                dict_str['text'] = dict_str['text'].translate(str.maketrans('', '', string.punctuation))  # removing punctuation
+                video_comments_list.append(dict_str)
+                print('done')
+                #video_comments_list.append(ast.literal_eval(line))
         video_comments_list_text = [comments_dict['text'] for comments_dict in video_comments_list]
 
-    except FileNotFoundError:
-        print(video_id)
+    except:
         video_id_list_with_no_comments.append(video_id)
 
     row['paragraphs'] = video_comments_list_text
@@ -40,3 +48,4 @@ filtered_video_df_with_comments = filtered_video_df.apply(lambda x: get_paragrap
 filtered_video_df_with_comments.to_csv(FILTERED_DATA_DIR+os.sep+"qna_data"+os.sep+CHANNEL+"_video_qna_data.csv",index=False)
 
 ######################
+filtered_video_df_with_comments = pd.read_csv(FILTERED_DATA_DIR+os.sep+"qna_data"+os.sep+CHANNEL+"_video_qna_data.csv")
